59d7f326c9590b6234d2a5cf02daf69fe63652d6
kumar <santanupaikaray1996@gmail.com>
2021-04-25 15:25:58 +0530

    add all changes

---
+++ 1.console/console.js
@@ -0,0 +1,6 @@
+
+console.log('This is log message')
+console.info('This is infor message')
+console.warn('This is warning ')
+console.error('This is an error')
+console.table({name:"santanu",city:"Bhubaneswar"})
---
+++ 2.hoisting/1.hoisting.js
@@ -0,0 +1,27 @@
+// var a=10;
+// var b=5;
+
+// function c(){
+//     console.log('function c is called')
+// }
+// function d(){
+//     console.log('function d is called')
+// }
+console.log(a)
+console.log(b)
+c()
+d()
+
+var a=10;
+var b=5;
+
+function c(){
+    console.log('function c is called')
+}
+function d(){
+    console.log('function d is called')
+}
+console.log(a)
+console.log(b)
+
+
